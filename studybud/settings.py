04e5a686c4cf92b77224857cd31f9554f71e9896
Jacob Crown <jkorona.pl@gmail.com>
2021-11-30 16:20:38 +0100

    database config

--- studybud/settings.py
+++ studybud/settings.py
@@ -87,7 +87,10 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': env.dj_db_url("DATABASE_URL")
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
